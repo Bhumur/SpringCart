49f405434adc6833fdc71b4ebf67da2fac878bc5
Bhumur Agrawal <57053398+Bhumur@users.noreply.github.com>
2025-08-11 21:18:29 +0530

    Delete StringCart directory

--- StringCart/src/main/java/com/example/StringCart/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.StringCart.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.StringCart.Entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
